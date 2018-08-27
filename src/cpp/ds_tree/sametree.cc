9ac9b3b7a29a532ad6ecb7647adc05a4f92d3ecc
Aman Vaishya <exa00039@exa00068.exadatum.local>
2018-08-27 22:28:04 +0530

    added solution for sametree, all solutions to the rest of the data structures and algorithms fom now onwards would be added to the respective directories, SAMETREE incomplete solution, to be merged to master after testing

---
+++ src/cpp/ds_tree/sametree.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p-> val != q-> val)  return false;
+        if(p && q){
+            isSameTree(p->left,q->left);
+            isSameTree(p->right,q->right);
+        }
+        return true;
+    }
+};
